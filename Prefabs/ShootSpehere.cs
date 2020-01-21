fd54f8021446f221248d49183228187dbefc8041
book81able <ecobb81@gmail.com>
2020-01-20 19:12:09 -0500

    First Draft

---
+++ Prefabs/ShootSpehere.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootSpehere : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject sphere;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            GameObject projectile;
+            projectile = Instantiate(sphere, transform.position + new Vector3(0,0,0),transform.rotation);
+        }
+    }
+}
---
+++ Prefabs/Zoom.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody zoo = GetComponent<Rigidbody>();
+        zoo.velocity = transform.TransformDirection(Vector3.forward * 10);
+    }
+}
