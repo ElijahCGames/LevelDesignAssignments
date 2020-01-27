441ba927cdef1fde2bbfc1d9ff51a0757624c795
book81able <ecobb81@gmail.com>
2020-01-27 18:36:18 -0500

    Jump Parti And Kill Plane System

---
+++ Scripts/KillPlane.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlane : MonoBehaviour
+{
+    public Transform checkpoint;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.position = checkpoint.position;
+        transform.rotation = checkpoint.rotation;
+    }
+
+}
