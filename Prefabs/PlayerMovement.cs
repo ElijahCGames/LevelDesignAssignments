36262740dd66d6de782d14e8c13077dbf794ad81
JoberXeven <45186143+JoberXeven@users.noreply.github.com>
2020-01-29 14:47:15 -0500

    FPS Adjustments
    Adjustments to FPS Controller

--- Prefabs/PlayerMovement.cs
+++ Prefabs/PlayerMovement.cs
@@ -39,10 +39,10 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded) {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
-
+        
         velocity.y += gravity * Time.deltaTime ;
         controller.Move(velocity * Time.deltaTime);
     }
