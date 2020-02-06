853556dedea7415c2f942bce20645dda1a78a9c4
book81able <ecobb81@gmail.com>
2020-02-06 16:44:35 -0500

    Material fixes

---
+++ Scripts/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float ShootRate = .01f;
+    public GameObject sphere;
+    public GameObject target;
+    private float timer;
+    private bool BossAlive = true;
+
+    private void Update()
+    {
+        if(target != null)
+        {
+            transform.LookAt(target.transform);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            target = other.gameObject;
+            StartCoroutine(shoot());
+        }
+
+    }
+
+    private IEnumerator shoot()
+    {
+        while (BossAlive)
+        {
+            yield return new WaitForSeconds(ShootRate);
+            Instantiate(sphere, transform.position + new Vector3(0f, 0, 2f), transform.rotation);
+        }
+    }
+}
