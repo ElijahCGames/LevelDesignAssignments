d5e4313f0f2f0a3abc059cc531f2e66e6287f85e
JoberXeven <45186143+JoberXeven@users.noreply.github.com>
2020-02-06 16:05:29 -0500

    BrokeMachineBreaksRight

--- LevelControl.cs
+++ LevelControl.cs
@@ -6,14 +6,16 @@ public class LevelControl : MonoBehaviour
 {
     public GameObject[] PillersMid;
     public GameObject[] PillersOut;
-    private bool midDes = false;
-    private bool outDes = false;
+    private bool midDes;
+    private bool outDes;
     private float timer = 0;
+    public GameObject bp;
     // Start is called before the first frame update
 
     void Start()
     {
-        
+        midDes = false;
+        outDes = false;
     }
 
     // Update is called once per frame
@@ -24,13 +26,19 @@ public class LevelControl : MonoBehaviour
         if(timer >= 10 && !midDes) {
             foreach(GameObject g in PillersMid) {
                 Destroy(g);
+                GameObject broke;
+                broke = Instantiate(bp, g.transform.position + new Vector3(0f, -2.5f, 0f), g.transform.rotation);
             }
+            midDes = true;
         }
 
         if (timer >= 20 && !outDes) {
             foreach (GameObject g in PillersOut) {
                 Destroy(g);
+                GameObject broke;
+                broke = Instantiate(bp, g.transform.position + new Vector3(0f, -2.5f, 0f), g.transform.rotation);
             }
+            outDes = true;
         }
 
     }
