b039c8787e9e6d14c23ace25eb20b255132bf321
book81able <ecobb81@gmail.com>
2020-02-13 14:54:27 -0500

    Camera and Sphere

---
+++ Scripts/CameraSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSet : MonoBehaviour
+{
+    public Transform sphere;
+    private Vector3 distance;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform startTransform = sphere;
+        distance = gameObject.transform.position - startTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = sphere.position + distance;
+    }
+}
---
+++ Scripts/SphereMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMove : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        Vector3 vect = new Vector3(vert * speed, 0, -horiz * speed);
+        rb.AddForce(vect);
+    }
+}
