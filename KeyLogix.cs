c3568ef6a1f05634174654466ea334534e6d347f
JoberXeven <45186143+JoberXeven@users.noreply.github.com>
2020-02-20 13:35:21 -0500

    Lock and Key

---
+++ KeyLogix.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyLogix : MonoBehaviour
+{
+    public GameObject targetAfter;
+    public GameObject targetBefore;
+    public GameObject targetDrop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetAfter.SetActive(false);
+        targetBefore.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void drop() {
+        targetAfter.SetActive(false);
+        targetDrop.SetActive(true);
+
+    }
+
+    public void pickUp() {
+        targetAfter.SetActive(true);
+        targetBefore.SetActive(false);
+    }
+}
---
+++ PlayerLockKey.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLockKey : MonoBehaviour
+{
+    private GameObject key;
+
+    private GameObject potentialKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Key: " +key);
+        Debug.Log("potKey: " + potentialKey);
+
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (potentialKey != null && key == null) {
+                key = potentialKey;
+                potentialKey = null;
+                key.transform.SetParent(transform);
+                key.GetComponent<KeyLogix>().pickUp();
+
+            } else if(key != null ) {
+                key.transform.SetParent(null);
+                key.GetComponent<KeyLogix>().drop();
+                key = null;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Key") && key == null) {
+            potentialKey = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Key")) {
+            potentialKey = null;
+        }
+    }
+
+    /*private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Key")) {
+            potentialKey = collision.gameObject;
+        }
+    }*/
+
+
+}
