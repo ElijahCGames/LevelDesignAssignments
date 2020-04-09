2d7745e0ff4d088d58bb2ac723556ee555dccdad
book81able <ecobb81@gmail.com>
2020-04-09 14:59:45 -0400

    Camera Shift

--- Scripts/MouseLook.cs
+++ Scripts/MouseLook.cs
@@ -10,8 +10,13 @@ public class MouseLook : MonoBehaviour
     public Transform aim;
     public GameObject TopDown;
     public GameObject ThirdPerson;
+
+    public GameObject normalTerrain;
+    public GameObject smellTerrain;
     float xRotation = 0f;
     bool NormalView = true;
+
+    public float cameraRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,15 +35,24 @@ public class MouseLook : MonoBehaviour
             {
                 NormalView = false;
                 gameObject.transform.parent = TopDown.transform;
-                gameObject.transform.localPosition = new Vector3(0, 0, 0);
-                transform.localRotation = Quaternion.Euler(80, 0f, 0f);
+                StartCoroutine(moveCameraToTop());
+                //gameObject.transform.localPosition = new Vector3(0, 0, 0);
+
+                ///transform.localRotation = Quaternion.Euler(80, 0f, 0f);
+
+                normalTerrain.SetActive(false);
+                smellTerrain.SetActive(true);
             }
             else
             {
                 NormalView = true;
                 gameObject.transform.parent = ThirdPerson.transform;
-                gameObject.transform.localPosition = new Vector3(0, 0, 0);
-                transform.localRotation = Quaternion.Euler(30, 0f, 0f);
+                StartCoroutine(moveCameraToThird());
+                //gameObject.transform.localPosition = new Vector3(0, 0, 0);
+                //transform.localRotation = Quaternion.Euler(30, 0f, 0f);
+
+                normalTerrain.SetActive(true);
+                smellTerrain.SetActive(false);
             }
         }
         float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
@@ -51,5 +65,26 @@ public class MouseLook : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);*/
 
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    IEnumerator moveCameraToTop()
+    {
+        Debug.Log(Vector3.Distance(transform.position, TopDown.transform.position) < 0.01f && NormalView == false);
+        while (Vector3.Distance(transform.position,TopDown.transform.position) > 0.01f && NormalView == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, TopDown.transform.position, 1);
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(80, 0f, 0f), cameraRotation);
+            yield return null;
+        }
+    }
+
+    IEnumerator moveCameraToThird()
+    {
+        while (Vector3.Distance(transform.position, ThirdPerson.transform.position) > 0.01f && NormalView == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, ThirdPerson.transform.position, 1);
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(30, 0f, 0f), cameraRotation);
+            yield return null;
+        }
     }
 }
